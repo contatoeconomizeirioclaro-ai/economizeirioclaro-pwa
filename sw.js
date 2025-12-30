43513637455c227c4c0c6eecbef1443209e36395
contatoeconomizeirioclaro-ai <contato.economizeirioclaro@gmail.com>
2025-12-30 19:31:13 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,17 @@
-const CACHE_NAME = 'economizei-v1';
-const OFFLINE_URL = '/offline.html';
-
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache =>
-      cache.addAll([
+    caches.open('economizei-v1').then(cache => {
+      return cache.addAll([
         '/',
-        OFFLINE_URL
-      ])
-    )
+        '/index.html',
+        '/manifest.json'
+      ]);
+    })
   );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match(OFFLINE_URL))
-    );
-  }
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match('/index.html'))
+  );
 });
